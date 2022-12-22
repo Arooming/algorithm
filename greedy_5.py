045d47f5f42eb0eb662c385333a685ddae34a38e
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2022-12-22 14:54:04 +0900

    Update greedy_5.py

--- greedy_5.py
+++ greedy_5.py
@@ -1,12 +1,17 @@
 # N: 도시의 개수
 N = int(input())
+
 # distance: 도시간의 거리
 distance = list(map(int, input().split()))
+
 # price: 주유소의 리터당 가격
 price = list(map(int, input().split()))
+
 # min_price: 도시 이동에 필요한 최소비용
 min_price = 0
+
 m = price[0]
+
 # 이전 주유 비용이 현재 주유 비용보다 많이 든다면, m에 현재 주유 비용 값 부여
 for i in range (N-1) :
     if (m > price[i]) :
